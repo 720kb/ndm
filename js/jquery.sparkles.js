d62be1df39871d39f57c1f97097286d26ede4848
45kb <filippo.oretti@gmail.com>
2016-05-14 18:28:33 +0200

    clean up

--- js/jquery.sparkles.js
+++ js/jquery.sparkles.js
@@ -2,6 +2,21 @@
 * Courtesy of http://codepen.io/simeydotme/pen/jgcvi
 *
 */
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+    // Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if (!isEdge && !isSafari) {
 
 $(function() {
 // default is varying levels of transparent white sparkles
@@ -21,6 +36,7 @@ $(".sparkley:last").sparkleh({
 // speed allows us to control... the ... velocity
 
 });
+}
 $.fn.sparkleh = function( options ) {
 
 return this.each( function(k,v) {

a47d96e8e32f3cb4ef85f2c9c4a313b0a138fa13
45kb <filippo.oretti@gmail.com>
2016-11-07 10:42:52 +0100

    minify dist task

---
+++ conf/tasks/minify.js
@@ -0,0 +1,31 @@
+/*global require*/
+(function buildTask() {
+  'use strict';
+
+  const gulp = require('gulp')
+    , runSequence = require('run-sequence')
+    , paths = require('../paths.json')
+    , minifyJS = require('gulp-uglify')
+    , minifyCSS = require('gulp-clean-css');
+
+  gulp.task('distify', ['dist'], done => {
+
+    runSequence([
+      'dist-minify-css',
+      'dist-minify-js'
+    ]);
+  });
+
+  gulp.task('dist-minify-css', () => {
+
+    return gulp.src(`${paths.dist}css/*.css`)
+      .pipe(minifyCSS())
+      .pipe(gulp.dest(`${paths.dist}css/`));
+  });
+  gulp.task('dist-minify-js', () => {
+
+    return gulp.src(`${paths.dist}js/*.js`)
+      .pipe(minifyJS())
+      .pipe(gulp.dest(`${paths.dist}js/`));
+  });
+}());

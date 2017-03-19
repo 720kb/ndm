a2bb70c892fe9116fb1b46fe4ffe80ae57ccb464
wouldgo <wouldgo84@gmail.com>
2017-03-19 22:00:22 +0100

    fix for rpm

--- conf/linux.sh
+++ conf/linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl -F package=@releases/ndm_$(echo $1)_amd64.deb https://$2@push.fury.io/720kb/ && \
-curl -F package=@releases/ndm_$(echo $1).rpm https://$2@push.fury.io/720kb/ && \
+curl -F package=@releases/ndm-$(echo $1).rpm https://$2@push.fury.io/720kb/ && \
 
 echo "Done."

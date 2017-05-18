19991091c72419106db3e30708e29b2de38c53d5
45kb <filippo.oretti@gmail.com>
2017-05-18 17:20:42 +0200

    was too much less

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ if (process.platform === 'win32') {
 //Set main window height smaller for Linux ONLY
 if (process.platform !== 'win32' &&
   process.platform !== 'darwin') {
-  applicationTemplate.minHeight -= 45;
-  applicationTemplate.height -= 45;
+  applicationTemplate.minHeight -= 20;
+  applicationTemplate.height -= 20;
 }
 app.on('window-all-closed', () => {
   app.quit();
